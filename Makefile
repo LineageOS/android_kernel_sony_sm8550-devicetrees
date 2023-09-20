6170a0ee4296c21519ec96422e17b3335438ab17
Luofan Chen <clfbbn@gmail.com>
2023-09-20 19:57:27 +0800

    qcom: Import changes from sony copyleft release
    [someone5678]
    * Replicate structure to oneplus for orginize
    As of:
    https://github.com/LineageOS/android_kernel_oneplus_sm8550-devicetrees/tree/f549953802810be37d698ea564a3b0257d1b8ac6
    
    * Remove useless guards and matches dtb inclusion to stock
    From:
    Sony/XQ-DQ54_EEA/XQ-DQ54:15/67.2.A.2.60/067002A002006000521143226:user/release-keys
    
    Change-Id: Ie4a5007f6cfecdcb25eac348495597a7c4ba3eee
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ vendor := $(srctree)/$(src)
 ifneq "$(wildcard $(vendor)/qcom)" ""
 	subdir-y += qcom
 endif
+#SOMC_DTS_OVERLAY start
+subdir-y += sony
+#SOMC_DTS_OVERLAY end
--- qcom/camera/config/kalama.mk
+++ qcom/camera/config/kalama.mk
@@ -1,4 +1,6 @@
 dtbo-$(CONFIG_ARCH_KALAMA) := kalama-camera.dtbo
+
+ifneq ($(CONFIG_MACH_SONY_DEVICE), y)
 dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-mtp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
@@ -9,3 +11,7 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
 				crow-camera-sensor-idp.dtbo \
 				crow-camera-sensor-qrd.dtbo
+endif
+
+dtb-$(CONFIG_MACH_SONY_DEVICE) += kalama-camera.dtbo
+dtbo-$(CONFIG_MACH_SONY_PDX234) += sony/kalama-camera-sensor-pdx234.dtbo
